a84351747cc8966ef07ae9aee8a196ab030a2f8c
Jaime <jaimemoravallejo@gmail.com>
2020-03-14 10:40:23 +0100

    5 User Model create

---
+++ models/User.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  name: String,
+  rol:[],
+  profile_image: String,
+  description: String,
+  dni: String,
+  Review: [{
+    content: {
+      type: String,
+      required: true
+    },
+    valoration: Number,
+    userid: {
+      type: String,
+      required: true
+    },
+  }]
+});
+
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
