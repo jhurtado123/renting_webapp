3634ba59a30971e1531ee8db841f794b9f1512ae
Your Name <hurtadojose89@gmail.com>
2020-03-14 11:28:18 +0100

    [23] - register page and methods

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 
+const session = require('express-session');
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
+const flash = require('connect-flash');
 
 
 const indexRouter = require('./routes/index');
@@ -29,7 +31,9 @@ app.set('view engine', 'hbs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('secret'));
+app.use(session({cookie: { maxAge: 60000 }}));
+app.use(flash());
 
 app.use(require('node-sass-middleware')({
   src:  path.join(__dirname, 'public'),
