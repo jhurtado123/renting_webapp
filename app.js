3d30fa3e7ccba8e5e861b3851a04132e3f90e8a9
Your Name <hurtadojose89@gmail.com>
2020-03-19 20:03:48 +0100

    [#33] - 500 and 400 error pages

--- app.js
+++ app.js
@@ -92,9 +92,11 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = process.env.ENV === 'development' ? err : {};
   // render the error page
   res.status(err.status || 500);
+
+  if (res.statusCode === 500) res.render('error500');
   res.render('error');
 });
 
