151d83da723f3aee52df1587632915f7f9f093f7
Your Name <hurtadojose89@gmail.com>
2020-03-19 11:29:25 +0100

    [#40] - add slider as component an reuse it on ad/view

--- public/javascripts/ad/view.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('.image-preview').forEach(element => {
-  element.addEventListener('click', function() {
-    document.querySelector('.slide.active').classList.remove('active');
-    const previewIndex = this.getAttribute('data-index');
-    document.querySelector(`.slide[data-index="${previewIndex}"]`).classList.add('active');
-  })
-});
---
+++ public/javascripts/components/slider.js
@@ -0,0 +1,33 @@
+var slideIndex = 0;
+showSlides();
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  if (slideIndex >= slides.length) {
+    slideIndex = 0;
+  }
+  if (slideIndex < 0) {
+    slideIndex = slides.length - 1;
+  }
+  slides[slideIndex].style.display = "block";
+}
+function plusSlides(n) {
+  slideIndex += n;
+  console.log(slideIndex);
+  showSlides();
+}
+
+function goToIndex(index) {
+  slideIndex = index;
+  showSlides();
+}
+
+document.querySelectorAll('.image-preview').forEach(element => {
+  element.addEventListener('click', function () {
+    const index = this.getAttribute('data-index');
+    goToIndex(parseInt(index));
+  });
+});
