dd6c29d63e4bad7988e55bd7ff4f6ae5338dcae9
Your Name <hurtadojose89@gmail.com>
2020-03-30 17:59:46 +0200

    [#85] - notify when an appointmnent is finished and then user can review lessee

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const flash = require('connect-flash');
 var hbs = require('hbs');
 const extend = require('handlebars-extend-block');
 const authMiddleware = require('./helpers/auth');
+const appointmentsStatus = require('./helpers/appointmentsStatus');
 
 
 hbs = extend(hbs);
@@ -102,65 +103,20 @@ app.dynamicHelpers({
 });
 
 app.use('/', indexRouter);
-app.use('/users', authMiddleware.checkIfUserLoggedIn, usersRouter);
-app.use('/ad', authMiddleware.checkIfUserLoggedIn, adsRouter);
-app.use('/appointment', authMiddleware.checkIfUserLoggedIn, appointmentsRouter);
-app.use('/chats', authMiddleware.checkIfUserLoggedIn, chatsRouter);
-app.use('/support', authMiddleware.checkIfUserLoggedIn, supportRouter);
-app.use('/review', authMiddleware.checkIfUserLoggedIn, reviewRouter);
 
-app.use('/api', apiRouter); //TODO authMiddleware.checkIfUserLoggedIn ?
+app.use(authMiddleware.checkIfUserLoggedIn);
+app.use(appointmentsStatus.changeAppointmentStatusIfFinished);
+app.use('/users', usersRouter);
+app.use('/ad', adsRouter);
+app.use('/appointment', appointmentsRouter);
+app.use('/chats', chatsRouter);
+app.use('/support', supportRouter);
+app.use('/review', reviewRouter);
 
-//register partials
+app.use('/api', apiRouter);
+
 hbs.registerPartials(path.join(__dirname, '/views/partials'));
-hbs.registerHelper('ifEq', function (arg1, arg2, options) {
-  return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
-});
-hbs.registerHelper("formatDate", function (datetime) {
-  return `${datetime.getUTCDate()}/${datetime.getUTCMonth() + 1}/${datetime.getFullYear()}`;
-});
-hbs.registerHelper("formatHours", function (datetime) {
-  return `${datetime.getHours()}:${datetime.getMinutes()}`;
-});
-hbs.registerHelper('switch', function(value, options) {
-  this.switch_value = value;
-  return options.fn(this);
-});
-hbs.registerHelper('case', function(value, options) {
-  if (value == this.switch_value) {
-    return options.fn(this);
-  }
-});
-hbs.registerHelper('ifIn', function(elem, list, options) {
-  if(list.indexOf(elem+'') !== -1) {
-    return options.fn(this);
-  }
-  return options.inverse(this);
-});
-hbs.registerHelper("getUnreadedNotifications", function (notifications) {
-  let count = 0;
-  notifications.forEach(notification => {
-    if (!notification.isReaded) count++;
-  });
-  return count;
-});
-hbs.registerHelper("fairPricePercent", function (fairPrice, price) {
-  if (!fairPrice || !price) return;
-  let percentDifference, result;
-  if (fairPrice > price) {
-    percentDifference = -(Math.round(((price - fairPrice) / price * 100)));
-  } else {
-    percentDifference = Math.round(((price - fairPrice) / price * 100));
-  }
-  if (fairPrice < price && percentDifference >= 5){
-    result = `<div class="fairPrice expensive"><i class="fa fa-arrow-up"></i>El piso es un ${percentDifference}% mas caro de lo recomendado</div>`;
-  } else if ( fairPrice > price && percentDifference >= 5) {
-    result = `<div class="fairPrice cheap"><i class="fa fa-arrow-down"></i>El piso es un ${percentDifference}% más barato de lo recomendado</div>`;
-  } else {
-    result = `<div class="fairPrice ok">El precio del piso está dentro la cantidad recomendada</div>`;
-  }
-  return result;
-});
+require('./helpers/appHbsHelpers')(hbs);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
